f3ccc9c14606c77e20cf05513697a1f8b7be2ffe
Lina-Dong <59942753+Lina-Dong@users.noreply.github.com>
2021-04-26 04:38:56 -0400

    add UI pages(footer, MyBlogs, Favorite, BlogEditor)

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+
+class Footer extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render() {
+		return(
+			<div className="footer">
+                <div className="container">
+                    <div className="row justify-content-center">             
+                        <div className="col-4 col-sm-2">
+                            <h5 className="offset-4">Contact</h5>
+                            <p className="m-0">pxh257@case.edu</p>
+                            <p className="m-0">txd172@case.edu</p>
+                            <p className="m-0">qxl216@case.edu</p>
+                            <p className="m-0">sxl1350@case.edu</p>
+                        </div>
+                        <div className="col col-sm">
+                        </div>
+                    </div>
+                    <div className="row justify-content-center">             
+                        <div className="col-auto">
+                            <p className="m-0">© Copyright 2021 CSDS393 Group 3</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		);
+	}
+}
+
+export default Footer;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import BlogViewer from './BlogViewerComponent';
+import MyBlogs from './MyBlogsComponent';
+import Favorite from './FavoriteComponent';
+import BlogEditor from './BlogEditorComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 
 const UsernameContext = React.createContext('');
@@ -27,7 +31,13 @@ class Main extends Component{
                     <Header history={this.props.history} onUsernameChange={this.onUsernameChange} username={this.state.username} authenticated={this.state.authenticated}/>
                     <Switch>
                         <Route exact path="/home" component={()=><Home history={this.props.history} location={this.props.location} username={this.state.username} authenticated={this.state.authenticated} onUsernameChange={this.onUsernameChange}/>} />
+                        <Route exact path="/favorite" component={Favorite} />
+                        <Route exact path="/myBlogs" component={MyBlogs} />
                         <Route path="/blogviewer/:blogId" component={() => <BlogViewer history={this.props.history} location={this.props.location} username={this.state.username} authenticated={this.state.authenticated} />}/>
+                        <Route path="/favorite/:username" component={()=><Favorite username={this.state.username} authenticated={this.state.authenticated} />} />
+                        <Route path="/myBlogs/:username" component={()=><MyBlogs username={this.state.username} authenticated={this.state.authenticated} />} />
+                        <Route exact path="/blogEditor" component={BlogEditor} />
+                        <Route path="/blogEditor/:blogId" component={()=><BlogEditor history={this.props.history} location={this.props.location} username={this.state.username} authenticated={this.state.authenticated} />} />
                         <Redirect to="/home" />
                     </Switch>
                 </UsernameContext.Provider>
